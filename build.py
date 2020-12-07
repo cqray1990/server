137f4698a1568d99786b61a85b6e9d8cbfbc204e
David Goodwin <davidg@nvidia.com>
2020-12-07 10:29:50 -0800

    short option

--- build.py
+++ build.py
@@ -1220,7 +1220,6 @@ if __name__ == '__main__':
         help='Minimum CUDA compute capability supported by server.')
 
     parser.add_argument(
-        '-e',
         '--endpoint',
         action='append',
         required=False,
@@ -1235,7 +1234,6 @@ if __name__ == '__main__':
         'Include specified filesystem in build. Allowed values are "gcs", "azure_storage" and "s3".'
     )
     parser.add_argument(
-        '-b',
         '--backend',
         action='append',
         required=False,
@@ -1243,7 +1241,6 @@ if __name__ == '__main__':
         'Include specified backend in build as <backend-name>[:<repo-tag>]. If <repo-tag> starts with "pull/" then it refers to a pull-request reference, otherwise <repo-tag> indicates the git tag/branch to use for the build, default is "main".'
     )
     parser.add_argument(
-        '-r',
         '--repo-tag',
         action='append',
         required=False,
